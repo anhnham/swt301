5cea83dda797c51de483ece0db39d9cf2c0bd31e
anhnham <128161951+anhnham@users.noreply.github.com>
2024-09-27 21:30:57 +0700

    Update Student.java

--- src/main/java/com/luv2code/cruddemo/entity/Student.java
+++ src/main/java/com/luv2code/cruddemo/entity/Student.java
@@ -13,13 +13,13 @@ public class Student {
     private int id;
 
     @Column(name="first_name")
-    private String firstName;
+    private string firstName;
 
     @Column(name="last_name")
-    private String lastName;
+    private string lastName;
 
     @Column(name="email")
-    private String email;
+    private string email;
 
     // define constructors
     public Student() {
